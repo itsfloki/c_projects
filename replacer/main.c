e150c74a16d6e6bd41d1a7871218615c1e7e69eb
itsfloki <coder.abhijit90@gmail.com>
2023-11-23 12:16:41 +0530

    update: parse cli arguments

--- replacer/main.c
+++ replacer/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello World\n");
+// cli arguments
+typedef struct {
+    char *find;
+    char *replace;
+    char *file_name;
+} Config;
+
+int main(int argc, char *argv[]) {
+    Config config;
+
+    if(argc < 4) {
+        printf("Arguments missing.\n");
+        exit(1);
+    }
+
+    config.find = *(++argv);
+    config.replace = *(++argv);
+    config.file_name = *(++argv);
+
+    printf("find: %s\n", config.find);
+    printf("replace: %s\n", config.replace);
+    printf("file: %s\n", config.file_name);
 
     return 0;
 }
