ae5a9a33e2516ffc834f23f40efcaee8ca0d8dca
itsfloki <coder.abhijit90@gmail.com>
2024-01-06 19:39:56 +0530

    add: rmeove and view

--- passman/main.c
+++ passman/main.c
@@ -10,9 +10,11 @@ typedef struct {
 
 void generate(char*, int);
 int write_store(Store);
-int read_store();
+int read_store(char*);
+int delete_store(char*);
 
 char* INVENTORY_FILE = "test.bin";
+char* TEMP_INVENTORY_FILE = "temp.bin";
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
@@ -44,8 +46,9 @@ int main(int argc, char* argv[]) {
         return write_store(store);
 
     } else if(strcmp(argv[1], "view") == 0) {
-        return read_store();
+        return read_store(argv[2]);
     } else if(strcmp(argv[1], "remove") == 0) {
+        return delete_store(argv[2]);
     }
 
     return 0;
@@ -69,7 +72,8 @@ int write_store(Store store) {
     FILE* fptr = fopen(INVENTORY_FILE, "a");
 
     if(fptr == NULL) {
-        printf("Err file opening\n");
+        printf("not able to open file\n");
+        return -1;
     }
 
     fwrite(&store, sizeof(Store), 1, fptr);
@@ -78,19 +82,46 @@ int write_store(Store store) {
 }
 
 // read all passwords from inventory file
-int read_store() {
+int read_store(char* label) {
     Store store;
     FILE* fptr = fopen(INVENTORY_FILE, "rb+");
 
     if(fptr == NULL) {
-        printf("Err file opening\n");
+        printf("not able to open file\n");
         return -1;
     }
 
     while(fread(&store, sizeof(Store), 1, fptr) == 1) {
-        printf("%s - %s\n", store.label, store.password);
+        if(strcmp(label, store.label) == 0) {
+            printf("%s\n", store.password);
+        }
     }
     
     fclose(fptr);
+    return 0;
+}
+
+int delete_store(char* label) {
+    Store store;
+    FILE* fptr = fopen(INVENTORY_FILE, "rb+");
+    FILE* temp_fptr = fopen(TEMP_INVENTORY_FILE, "w");
+
+    if(fptr == NULL || temp_fptr == NULL) {
+        printf("not able to open file\n");
+        return -1;
+    }
+
+    while(fread(&store, sizeof(Store), 1, fptr) == 1) {
+        if(strcmp(label, store.label) != 0) {
+            fwrite(&store, sizeof(Store), 1, temp_fptr);
+        }
+    }
+    
+    fclose(fptr);
+    fclose(temp_fptr);
+
+    remove(INVENTORY_FILE);
+    rename(TEMP_INVENTORY_FILE, INVENTORY_FILE);
+
     return 0;
 }
